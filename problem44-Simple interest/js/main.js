bea97eae85c5203f1722440d57a5b7888aeda1c4
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:35:45 +0530

    adding files.

---
+++ problem44-Simple interest/js/main.js
@@ -0,0 +1,9 @@
+var principal=prompt("enter the value of principal");
+principal=parseInt(principal);
+var rate=prompt("enter the value of rate");
+rate=parseInt(rate);
+var time=prompt("enter the value of time");
+time=parseInt(time);
+var simpleinterest;
+var simpleinterest=(principal*rate*time/100);
+alert(simpleinterest);
